06746215267b4327338c53a21345b2ffa2146c2e
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-05-28 20:53:10 -0400

    Creating node server

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.get('*', function(req, res, next) {
+  if(path.extname(req.path).length > 0) {
+    next()
+  }
+  else {
+    req.url = '/index.html'
+    next()
+  }
+});
+
+app.use(express.static('public'));
+
+var server = app.listen(process.env.PORT || 3000, function(){
+  console.log('Server running on PORT: ', server.address().port);
+});
