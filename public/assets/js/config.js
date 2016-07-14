72c99f5eb3d7f42bc1bdf0835270487399e0156f
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-07-14 04:19:53 -0400

    Trying to get cloudinary working with heroku

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,6 +1,1 @@
-fitnessSpotter.config(['cloudinaryProvider', function (cloudinaryProvider) {
-  // Configuring cloudinary api secrets and keys
-  cloudinaryProvider
-      .set("cloud_name", "hitv4slde")
-      .set("upload_preset", "ing8qiq8");
-}]);
+fitnessSpotter.config(process.env.CLOUDINARY_URL);
