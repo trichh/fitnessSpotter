d4858bd87eb588e7aa5d1ece9f74e53eaade8b47
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-07-14 03:28:25 -0400

    Trying to get cloudinary working with heroku

--- ng/module.js
+++ ng/module.js
@@ -1,1 +1,1 @@
-var fitnessSpotter = angular.module('fitnessSpotter', ['ngRoute', 'ngAnimate', 'cloudinary', 'ngFileUpload'])
+var fitnessSpotter = angular.module('fitnessSpotter', ['ngRoute', 'ngAnimate', 'ngFileUpload'])
