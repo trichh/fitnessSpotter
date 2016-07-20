4c41355372541a75ccba2c2f8f08c2c298cee7bf
Trichh <tylerrichard@Tylers-MacBook-Pro.local>
2016-07-19 22:14:09 -0400

    Trying to upload picture to cloudinary with heroku

--- ng/controllers/registerctrl.js
+++ ng/controllers/registerctrl.js
@@ -9,7 +9,7 @@ angular.module('fitnessSpotter').controller('RegisterCtrl', ['$scope', '$locatio
         var file = selectedFile[i];
         console.log("FILE:", file);
         console.log("FILE NAME", file.name);
-        $scope.photo = file.name;
+        $scope.photo = file;
         i++;
     }
   }
